83cd17da41663e4bf0bdb80617d34e47c3db5502
Fredericojunio <fredericoalbinojunior@gmail.com>
2026-03-18 14:30:14 +0200

    Inicialização do projeto com estrutura base e matriz de lugares

--- Codigo/main.cpp
+++ Codigo/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+const int FILAS = 10;
+const int COLUNAS = 4;
+
+void mostrarMapa(int lugares[FILAS][COLUNAS]) {
+    cout << "\nMapa de Lugares\n\n";
+
+    for (int i = 0; i < FILAS; i++) {
+        cout << "Fila " << i + 1 << ": ";
+        for (int j = 0; j < COLUNAS; j++) {
+            cout << lugares[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int lugares[FILAS][COLUNAS] = {0};
+
+    mostrarMapa(lugares);
+
+    return 0;
+}
